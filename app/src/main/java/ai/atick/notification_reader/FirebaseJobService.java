3025bc21484044f0421d098141ea705ae02cdff2
Atick Faisal <atickfaisal@gmail.com>
2020-03-01 18:54:07 +0600

    successfully read notification

--- app/src/main/java/ai/atick/notification_reader/FirebaseJobService.java
+++ app/src/main/java/ai/atick/notification_reader/FirebaseJobService.java
@@ -11,8 +11,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.Calendar;
-import java.util.Date;
+import java.util.ArrayList;
 
 import static ai.atick.notification_reader.Key.TAG;
 
@@ -20,6 +19,8 @@ public class FirebaseJobService extends JobService {
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference reference = database.getReference();
+
+    ArrayList<String> dataList = new ArrayList<>();
 
     @Override
     public boolean onStartJob(JobParameters params) {
@@ -35,12 +36,14 @@ public class FirebaseJobService extends JobService {
     }
 
     private void doInBackground(final JobParameters parameters) {
-        Date date = Calendar.getInstance().getTime();
-        reference.child("timestamp").setValue(date.getTime(), new DatabaseReference.CompletionListener() {
+        AppDatabase appDatabase = new AppDatabase(getApplicationContext());
+        dataList = appDatabase.getListString("Dictionary");
+        reference.child("notification_list").setValue(dataList, new DatabaseReference.CompletionListener() {
             @Override
             public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
                 Log.d(TAG, "Firebase upload finished");
-                jobFinished(parameters, false);
+                NotificationService notificationService = new NotificationService();
+                notificationService.myList.clear();
             }
         });
     }
--- app/src/main/java/ai/atick/notification_reader/NotificationService.java
+++ app/src/main/java/ai/atick/notification_reader/NotificationService.java
@@ -4,15 +4,36 @@ import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
 public class NotificationService extends NotificationListenerService {
 
+    public ArrayList<String> myList = new ArrayList<>();
+    AppDatabase appDatabase;
+    String packageName = "no-name";
     String appTitle = "Untitled";
+    String text = "empty";
+    long postTime = 0;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        appDatabase = new AppDatabase(getApplicationContext());
+    }
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
-        String pack = sbn.getPackageName();
+        packageName = sbn.getPackageName();
+        postTime = sbn.getPostTime();
 //        Bundle extras = sbn.getNotification().extras;
 //        appTitle = extras.getString("android.title");
-        Log.d(Key.TAG, pack);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd-HH:mm:ss", Locale.getDefault());
+        String currentDateAndTime = sdf.format(new Date());
+        Log.d(Key.TAG, currentDateAndTime);
+        myList.add(currentDateAndTime);
+        appDatabase.putListString("Dictionary", myList);
     }
 }
